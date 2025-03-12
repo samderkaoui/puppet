fd3689a04b228fb372472c1a2aaf816ee740018b
sderkaoui <sderkaoui>
2025-03-12 14:38:56 +0100

    fix indent

--- environments/production/manifests/defaults.pp
+++ environments/production/manifests/defaults.pp
@@ -16,6 +16,8 @@ node default {
 #S'applique UNIQUEMENT aux nodes qui n'ont PAS de définition spécifique dans nodes.pp
 #Ne s'applique PAS aux nodes explicitement définis (comme 'agent01' ou 'node2.example.com')
 #C'est un "filet de sécurité" pour les nodes inconnus ou non configurés
+
+
 #globals.pp (niveau supérieur, sans node) :
 #S'applique à TOUS les nodes, qu'ils soient définis explicitement ou non
 #Exécuté pour chaque node, sans exception

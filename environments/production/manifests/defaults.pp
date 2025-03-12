86c314a3a8fa2fa1900fac493d5e4e9bf8aadc0a
sderkaoui <sderkaoui>
2025-03-12 14:36:59 +0100

    add infos

--- environments/production/manifests/defaults.pp
+++ environments/production/manifests/defaults.pp
@@ -9,3 +9,14 @@ node default {
     message => "Ce node n'est pas explicitement défini dans Puppet.",
   }
 }
+
+
+
+#defaults.pp avec node default { ... } :
+#S'applique UNIQUEMENT aux nodes qui n'ont PAS de définition spécifique dans nodes.pp
+#Ne s'applique PAS aux nodes explicitement définis (comme 'agent01' ou 'node2.example.com')
+#C'est un "filet de sécurité" pour les nodes inconnus ou non configurés
+#globals.pp (niveau supérieur, sans node) :
+#S'applique à TOUS les nodes, qu'ils soient définis explicitement ou non
+#Exécuté pour chaque node, sans exception
+#Établit une configuration de base universelle

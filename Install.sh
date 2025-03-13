c6d6f8edec561d1c6cf38ed32fe7a9e3fe04b297
sderkaoui <sderkaoui>
2025-03-13 10:02:49 +0100

    add link to stephane robert

--- Install.sh
+++ Install.sh
@@ -1,4 +1,6 @@
 https://shape.host/resources/puppet-server-and-agent-installation-guide-debian-11
+https://blog.stephane-robert.info/docs/infra-as-code/gestion-de-configuration/puppet/introduction/
+
 
 # Prérequis ET debian 11
 # sudo passwd sderkaoui
@@ -150,3 +152,234 @@ sudo /opt/puppetlabs/bin/puppetserver ca clean --certname agent.localdomain.lan
 
 # Commande pour lancer puppet agent
 sudo /opt/puppetlabs/bin/puppet agent -t
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Confluence VPG :
+
+
+Installation d’un serveur Puppet en version 8 sur OS Debian (11.9 stable)
+
+Dans le cas où il s’agisse d’une machine livrée depuis un template plus ancien, enlever le repo ancien:
+
+apt-get remove --purge puppet-agent puppet6-release
+
+Install
+
+Inspiré par diverses sources:
+
+# https://libremaster.com/comment-installer-un-serveur-puppet-foreman-debian/
+# https://shape.host/resources/puppet-server-and-agent-installation-guide-debian-11
+
+Ajouter le repo v8:
+
+wget https://apt.puppet.com/puppet8-release-bullseye.deb
+dpkg -i puppet8-release-bullseye.deb
+apt-get update
+
+Install à proprement parler:
+
+apt-get install puppetserver puppetdb puppetdb-termini
+systemctl enable puppetserver
+
+Adapter l’allocation mémoire selon la quantité de RAM dans /etc/default/puppetserver :
+
+JAVA_ARGS="-Xms1g -Xmx1g -Djruby.logger.class=com.puppetlabs.jruby_utils.jruby.Slf4jLogger"
+
+
+
+Exemple du fichier /etc/puppetlabs/puppet/puppet.conf :
+
+[server]
+vardir = /opt/puppetlabs/server/data/puppetserver
+logdir = /var/log/puppetlabs/puppetserver
+rundir = /var/run/puppetlabs/puppetserver
+pidfile = /var/run/puppetlabs/puppetserver/puppetserver.pid
+codedir = /etc/puppetlabs/code
+
+[agent]
+server = vp-cp-officeit-puppetmain01.aix.vpg.lan
+
+[master]
+storeconfigs = true
+storeconfigs_backend = puppetdb
+
+PostgreSQL
+
+Configurer une base de données dans POTSGRESQL pour PuppetDB
+
+apt-get install postgresql-13
+su - postgres
+# PAS dans le prompt 'psql'
+createuser -DRSP puppetdb
+createdb -E UTF8 -O postgres puppetdb
+psql puppetdb -c 'revoke create on schema public from public'
+psql puppetdb -c 'grant create on schema public to puppetdb'
+psql puppetdb -c 'create extension pg_trgm'
+
+Customisation pgsql
+
+dans /etc/postgresql/13/main/postgresql.conf :
+
+# Add settings for extensions here
+listen_addresses = localhost
+
+dans pg_hba.conf , tel que :
+
+# Rule Name: local access as postgres user
+# Description: none
+# Order: 1
+local	all	postgres		ident
+# Rule Name: local access to database with same name
+# Description: none
+# Order: 2
+local	all	all		ident
+# Rule Name: allow localhost TCP access to postgresql user
+# Description: none
+# Order: 3
+host	all	postgres	127.0.0.1/32	md5
+# Rule Name: deny access to postgresql user
+# Description: none
+# Order: 4
+host	all	postgres	0.0.0.0/0	reject
+# Rule Name: allow access to all users
+# Description: none
+# Order: 100
+host	all	all	0.0.0.0/0	md5
+# Rule Name: allow access to ipv6 localhost
+# Description: none
+# Order: 101
+host	all	all	::1/128	md5
+
+NB: Selon le profil de la machine des options peuvent être finetunées avec https://pgtune.leopard.in.ua
+
+Add/modify this settings in postgresql.conf and restart database
+
+
+# DB Version: 13
+# OS Type: linux
+# DB Type: web
+# Total Memory (RAM): 4 GB
+# CPUs num: 2
+# Data Storage: ssd
+
+max_connections = 200
+shared_buffers = 1GB
+effective_cache_size = 3GB
+maintenance_work_mem = 256MB
+checkpoint_completion_target = 0.9
+wal_buffers = 16MB
+default_statistics_target = 100
+random_page_cost = 1.1
+effective_io_concurrency = 200
+work_mem = 2621kB
+huge_pages = off
+min_wal_size = 1GB
+max_wal_size = 4GB 
+
+Configurer puppetdb
+
+Dans /etc/puppetlabs/puppetdb/conf.d/database.ini :
+
+[database]
+# Db connection
+subname = //localhost:5432/puppetdb
+username = puppetdb
+password = puppetdb
+
+# How often (in minutes) to compact the database
+gc-interval = 60
+classname = org.postgresql.Driver
+subprotocol = postgresql
+syntax_pgs = true
+node-ttl = 7d
+node-purge-ttl = 14d
+report-ttl = 14d
+log-slow-statements = 10
+conn-max-age = 60
+conn-keep-alive = 45
+conn-lifetime = 0
+
+Activer la configuration SSL de PuppetDB: puppetdb ssl-setup
+
+Relancer PuppetDB: systemctl restart puppetdb
+
+Créer un fichier /etc/puppetlabs/puppet/puppetdb.conf :
+
+[main]
+server_urls = https://vp-cp-officeit-puppetmain01.aix.vpg.lan:8081
+
+
+
+Set the Puppet server domain name in the Puppet agent configuration:
+puppet config set server vp-cp-officeit-puppetmain01.aix.vpg.lan --section agent
+
+Relancer PuppetServer : systemctl restart puppetserver
+
+Install puppetboard
+
+# cf: https://github.com/voxpupuli/puppet-puppetboard
+# https://forge.puppet.com/modules/puppet/puppetboard/readme
+
+
+
+puppet module install puppet-puppetboard
+puppet module install puppetlabs-apache
+apt-get install python3-venv
+
+Manifest local:
+
+# cat /etc/puppetlabs/code/environments/production/manifests/init.pp
+node /vp-cp-officeit-puppetmain01/ {
+
+  # Configure Apache on this server
+  class { 'apache':
+    default_vhost => false,
+  }
+
+  # Configure Puppetboard
+  class { 'puppetboard':
+    secret_key     => fqdn_rand_string(32),
+  }
+
+  # Access Puppetboard through pboard.example.com
+  class { 'puppetboard::apache::vhost':
+    vhost_name => 'vp-cp-officeit-puppetmain01',
+    port       => 80,
+  }
+
+}
+#
+
+Run this local manifest with : puppet agent -t
+
+L’interface web devrait répondre sur l’url: http://vp-cp-officeit-puppetmain01.aix.vpg.lan
+
+
+
+
+
+Problèmes SSL
+
+Dans le cas de soucis d’enrollment , côté serveur:
+
+# puppetserver ca sign --all
+Successfully signed certificate request for sysrescue
+# puppetserver ca clean --certname vp-dc2-pim-sql02.dc.vpg.lan
+
+Côté client :
+
+# rm -rf /etc/puppetlabs/puppet/ssl/*
+# puppet agent -t
